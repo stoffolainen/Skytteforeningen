b5173e3b16b32b9239ab3abe35b533d259e7cd24
Christoffer Karlsson <christoffer.karlsson@invativa.se>
2018-08-20 22:15:21 +0200

    Add token issuer options

---
+++ Skytteforeningen/Api/Infrastructure/Auth/TokenIssuerOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Api.Infrastructure.Auth
+{
+    public class TokenIssuerOptions
+    {
+        public string Issuer { get; set; }
+
+        public string Audience { get; set; }
+
+        public string SigningSecret { get; set; }
+
+        public TimeSpan ValidFor { get; set; } = TimeSpan.FromDays(2);
+    }
+}
