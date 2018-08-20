1b9946642304b05ddfd637b2bed82676099e6d38
Christoffer Karlsson <christoffer.karlsson@invativa.se>
2018-08-20 22:16:23 +0200

    Add claims extensions

---
+++ Skytteforeningen/Api/Infrastructure/Auth/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Security.Claims;
+
+namespace Api.Infrastructure.Auth
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string UserId(this ClaimsPrincipal user)
+        {
+            return user.Claims.Single(x => x.Type == "sub").Value;
+        }
+    }
+}
