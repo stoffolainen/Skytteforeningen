898f7277a185650bd0b32b8d0fa08495ad5e9415
Christoffer Karlsson <christoffer.karlsson@invativa.se>
2018-08-20 22:11:10 +0200

    Add empty context

---
+++ Skytteforeningen/Domain/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User.User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
