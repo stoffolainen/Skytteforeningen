6dc7c7882da9c43a8c5c10cbc181065ebf59a5f6
Christoffer Karlsson <christoffer.karlsson@invativa.se>
2018-08-20 22:13:00 +0200

    Add datetime extensions

---
+++ Skytteforeningen/Api/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Api
+{
+    public static class DateTimeExtensions
+    {
+        public static long ToUnixEpochDate(this DateTime date)
+        {
+            var timeSpan = date.ToUniversalTime() -
+                           new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+            return (long)Math.Round(timeSpan.TotalSeconds);
+        }
+    }
+}
