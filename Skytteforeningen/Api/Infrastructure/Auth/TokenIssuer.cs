2e7b904987e89a9143da6c4cb006fc38b9a611fb
Christoffer Karlsson <christoffer.karlsson@invativa.se>
2018-08-20 22:16:01 +0200

    Add token issuer

---
+++ Skytteforeningen/Api/Infrastructure/Auth/TokenIssuer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain.User;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Api.Infrastructure.Auth
+{
+    public static class TokenIssuer
+    {
+        public static TokenResponse AccessToken(User user, TokenIssuerOptions options)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToUnixEpochDate().ToString(),
+                    ClaimValueTypes.Integer64)
+            };
+
+            var jwt = new JwtSecurityToken(
+                options.Issuer,
+                options.Audience,
+                claims,
+                DateTime.Now,
+                DateTime.Now.Add(options.ValidFor),
+                new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(options.SigningSecret)),
+                    SecurityAlgorithms.HmacSha256));
+
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return new TokenResponse(encodedJwt, jwt.ValidTo);
+        }
+    }
+}
