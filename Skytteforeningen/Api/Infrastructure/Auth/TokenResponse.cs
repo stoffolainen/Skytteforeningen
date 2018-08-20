1c7aa7e03317cc4c0e718c44ab3b93f8b4906404
Christoffer Karlsson <christoffer.karlsson@invativa.se>
2018-08-20 22:15:44 +0200

    Add token response

---
+++ Skytteforeningen/Api/Infrastructure/Auth/TokenResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Api.Infrastructure.Auth
+{
+    public class TokenResponse
+    {
+        [JsonProperty(PropertyName = "access_token")]
+        public string AccessToken { get; }
+
+        [JsonProperty(PropertyName = "expires_at")]
+        public DateTime ExpiresAt { get; }
+
+        public TokenResponse(string accessToken, DateTime expiresAt)
+        {
+            AccessToken = accessToken;
+            ExpiresAt = expiresAt;
+        }
+    }
+}
